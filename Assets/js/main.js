5ff53dd4bba645456b25cd0c64f8eb9388675249
andrewphm <andrew@Andrews-MacBook-Pro.local>
2021-06-27 13:44:28 -0400

    Add header change on scroll

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,9 +1,8 @@
-/*======== SHOW MENU ========*/
+/*==================== SHOW MENU ====================*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close');
 
-/*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if(navToggle){
     navToggle.addEventListener('click', () =>{
@@ -11,10 +10,37 @@ if(navToggle){
     });
 }
 
-/*===== MENU HIDDEN =====*/
+/*==================== HIDE MENU ====================*/
 /* Validate if constant exists */ 
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu');
     });
 }
+
+/*==================== REMOVE MENU MOBILE ====================*/
+// Use querySelectorAll to return an array of all elements with css class selector .nav__link
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+// Add eventlistener to each nav link element
+navLink.forEach(e => e.addEventListener('click', linkAction));
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    //check to see if window.scrollY
+    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/*==================== SWIPER DISCOVER ====================*/
+
+/*==================== VIDEO ====================*/
+
+/*==================== SHOW SCROLL UP ====================*/
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Tavel mockup page
 
+Mobile-first fully responsive website.
+
 Features
 - Custom framework 
 - Fully responsive
--- Side bar for tablet 
+- Side bar for tablet 
 - Animations 
 - Dark/Light mode
 - Strong focus on using CSS variables and relative units
@@ -11,3 +13,8 @@ Features
 
 Resources: 
 - [Remix icon](https://remixicon.com/)
+- Images
+   https://www.pexels.com/
+   https://unsplash.com/
+- Colours
+   https://www.w3schools.com/colors/colors_hsl.asp
