64f6d92a0ec0cf6e564bdb622d1bb5c413825f71
andrewphm <andrpham@gmail.com>
2021-06-28 21:45:07 -0400

    Finish mediaqueries and add scrollreveal

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -98,7 +98,6 @@ window.addEventListener('scroll', scrollUp)
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-console.log(sections);
 
 function scrollActive(){
     const scrollY = window.pageYOffset
@@ -122,32 +121,45 @@ window.addEventListener('scroll', scrollActive)
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'ri-sun-line'
+const themeName = document.getElementById('theme-name')
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
-}
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
     // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
+    //change theme name
+    if(themeName.innerText === 'Dark mode') {
+        themeName.innerText = 'Light mode'
+    } else if(themeName.innerText === 'Light mode'){
+        themeName.innerText = 'Dark mode'
+    }
 })
 
 
-/*==================== SCROLL REVEAL ANIMATION ====================*/
 
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2300,
+    reset: true,
+})
+
+sr.reveal(`.home__data, .home__social-link, .home__info,
+           .discover__container,
+           .experience__data, .experience__overlay,
+           .place__card, .sponsor__content, .footer__data, .footer__rights`, {
+    origin: 'top',
+    interval: 100, 
+});
+
+sr.reveal(`.about__data, .video__description, .subscribe__description`, {
+    origin: 'left', 
+})
+
+sr.reveal(`.about__img-overlay, .video__content, .subscribe__form`, {
+    origin: 'right',
+    interval: 100, 
+})
--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # Tavel mockup page
 
-Mobile-first fully responsive website.
+---
+## Case study
+Mobile-first fully responsive website with animations and light/dark mode. 
 
-Features
+## Features
 - Custom framework 
 - Fully responsive
-- Side bar for tablet 
-- Animations 
+- Side bar for small screen
+- Animations
 - Dark/Light mode
 - Strong focus on using CSS variables and relative units
-- Touch slider for mobile using SwiperJS (Effect coverflow style)
+- Touch slider using SwiperJS (Effect coverflow style)
+- Scroll reveal animations using ScrollRevealJs library
 
 
-Resources: 
+## Resources: 
 - [Remix icon](https://remixicon.com/)
 - [SwiperJS](https://swiperjs.com/) 
 - Images
@@ -20,3 +23,9 @@ Resources:
    https://unsplash.com/
 - Colours
    https://www.w3schools.com/colors/colors_hsl.asp
+- [ScrollRevealJS](https://scrollrevealjs.org/)
+
+
+![Preview](/preview.png)
+
+[Click here to view live](http://www.andrewpham.com/travel)
