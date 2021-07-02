6eb85c9142e73e4d40e88884879a6a62da95f7ef
andrewphm <andrpham@gmail.com>
2021-07-01 23:19:01 -0400

    fix scroll reveal

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -97,15 +97,15 @@ window.addEventListener('scroll', scrollUp)
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
-
+console.log(sections);
 
 function scrollActive(){
     const scrollY = window.pageYOffset
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+        const sectionTop = current.offsetTop - 20;
+        let sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
@@ -144,7 +144,7 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({
     distance: '60px',
     duration: 2300,
-    reset: true,
+    reset: false,
 })
 
 sr.reveal(`.home__data, .home__social-link, .home__info,
